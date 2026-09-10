80bfded276338c825965257ffc0f0c30c8ec5f94
cschrock1 <cullenschrock@gmail.com>
2026-09-10 12:41:45 -0400

    main.py fix

--- server/app/main.py
+++ server/app/main.py
@@ -12,7 +12,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=CORS_ORIGINS,
+    allow_origin_regex=r"http://localhost:\d+",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
